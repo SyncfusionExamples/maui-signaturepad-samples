3e1382abe18e352f7a4dbf37ee3da4b950f3a3e5
Brundha <brundha.sf3969@gmail.com>
2024-06-06 17:12:21 +0530

    Added samples for the KB Articles.

---
+++ ByteArraySignature/ByteArraySignature/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ByteArraySignature
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ByteArraySignature/ByteArraySignature/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace ByteArraySignature
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void SfSignaturePad_DrawCompleted(object sender, EventArgs e)
+        {
+            StreamImageSource streamImageSource = (StreamImageSource)signaturePad.ToImageSource();
+            System.Threading.CancellationToken cancellationToken =
+            System.Threading.CancellationToken.None;
+            Task<Stream> task = streamImageSource.Stream(cancellationToken);
+            Stream stream = task.Result;
+            byte[] bytes = new byte[stream.Length];
+            stream.Read(bytes, 0, bytes.Length);
+            MemoryStream memoryStream = new MemoryStream(bytes);
+            image.Source = ImageSource.FromStream(() => memoryStream);
+        }
+    }
+
+}
